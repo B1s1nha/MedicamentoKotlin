f4f5388065f0f2b150eee7f5f380ed06ab2748e8
Gabriella Huppes <b1s1nha_@hotmail.com>
2023-10-17 21:15:03 -0300

    app adroid/kotlin, versão inicial, interface não concluida, bugs pois ainda estou fazendo a parte das tasks e marcações

---
+++ app/src/main/java/com/example/medicamentoapp/ui/metas/MetasViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.medicamentoapp.ui.metas
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MetasViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is metas Fragment"
+    }
+    val text: LiveData<String> = _text
+}
